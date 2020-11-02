40b6417c1670f6e83365900305bafe059569797b
Stephen Lautier <stephen.lautier@outlook.com>
2020-11-02 23:07:33 +0100

    default strategy

---
+++ src/viewport/viewport-data/index.ts
@@ -0,0 +1,3 @@
+export { ViewportDataService } from "./viewport-data.service";
+export { ViewportDataPipe } from "./viewport-data.pipe";
+export { ViewportDataConfig, ViewportDataMatchStrategy, ViewportDataMatchStrategyLiteral } from "./viewport-data-matcher";
--- src/viewport/viewport-data/viewport-data.service.ts
+++ src/viewport/viewport-data/viewport-data.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { UX_CONFIG, UxOptions } from "../../config";
 
 import { ViewportSizeTypeInfo } from "../viewport.model";
 import { ViewportService } from "../viewport.service";
@@ -13,20 +14,20 @@ export class ViewportDataService {
 
 	constructor(
 		private viewport: ViewportService,
+		@Inject(UX_CONFIG) private config: UxOptions,
 	) {
 	}
 
 	/** Get data for match. */
 	get<T>(
 		dataConfig: ViewportDataConfig<T>,
-		strategy: ViewportDataMatchStrategy,
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		viewportSizeType: ViewportSizeTypeInfo = this.viewport.sizeTypeSnapshot!
+		strategy: ViewportDataMatchStrategy = this.config.viewport.defaultDataMatchStrategy,
+		viewportSizeType: ViewportSizeTypeInfo = this.viewport.sizeTypeSnapshot
 	): T | undefined {
 		return matchViewportData(dataConfig, viewportSizeType, strategy, this.viewport.sizeTypes, this.viewport.sizeTypeMap);
 	}
 
-	get$<T>(dataConfig: ViewportDataConfig<T>, strategy: ViewportDataMatchStrategy,): Observable<T | undefined> {
+	get$<T>(dataConfig: ViewportDataConfig<T>, strategy?: ViewportDataMatchStrategy): Observable<T | undefined> {
 		return this.viewport.sizeType$.pipe(
 			map(sizeType => this.get<T>(dataConfig, strategy, sizeType)),
 		);
