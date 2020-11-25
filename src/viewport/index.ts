f3d4a9bdf9c8c4c64e21016f4cb0f504e4d884ff
Stephen Lautier <stephen.lautier@outlook.com>
2020-11-25 15:03:40 +0100

    feat(viewport): implement ssvViewportMatcherVar (#26)

--- src/viewport/index.ts
+++ src/viewport/index.ts
@@ -1,8 +1,9 @@
 export * from "./viewport-data/index";
 
-export * from "./viewport-matcher.directive";
-export * from "./viewport-server-size.service";
+export { SsvViewportMatcherVarDirective, SsvViewportMatcherVarContext } from "./viewport-matcher-var.directive";
+export { SsvViewportMatcherDirective, SsvViewportMatcherContext } from "./viewport-matcher.directive";
+export { UX_VIEWPORT_SSR_DEVICE, ViewportServerSizeService } from "./viewport-server-size.service";
 export { ComparisonOperation, DeviceType, UxViewportOptions, ViewportSize, ViewportSizeType, ViewportSizeTypeInfo } from "./viewport.model";
-export * from "./viewport.service";
+export { ViewportService } from "./viewport.service";
 export { generateViewportSizeType } from "./viewport.util";
 export { UX_VIEWPORT_DEFAULT_BREAKPOINTS } from "./viewport.const";
